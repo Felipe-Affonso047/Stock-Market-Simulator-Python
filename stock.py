b3d827a4f29a320265a930d67ca8ba25b2e2e982
Felipe Affonso <felipe.affonso@mths.ca>
2021-05-05 01:26:59 +0000

    beta 1.3

--- stock.py
+++ stock.py
@@ -4,5 +4,41 @@
 # Created on: April 2021
 # This program has stocks
 
-company1_price = float(input())
-company1 = company1_price
+import random
+
+company1_price = 10
+company2_price = 5
+company3_price = 50
+company4_price = 40
+
+
+def flutuation():
+    global company1_price
+    global company2_price
+    global company3_price
+    global company4_price
+
+    company1_price = company1_price + 1 + random.randint(-1000, 1000) / 100
+    company2_price = company2_price + 0.5 + random.randint(-100, 100) / 100
+    company3_price = company3_price - 0.1 + random.randint(-1000, 1000) / 10
+    company4_price = company4_price + random.randint(-1000, 1000) / 100
+    
+
+def flutuation_test():
+    global company1_price
+    global company2_price
+    global company3_price
+    global company4_price
+
+    company1_price = company1_price + 1 + random.randint(-1000, 1000) / 100
+    company2_price = company2_price + 0.5 + random.randint(-100, 100) / 100
+    company3_price = company3_price - 0.1 + random.randint(-1000, 1000) / 10
+    company4_price = company4_price + random.randint(-1000, 1000) / 100
+    
+    print("{0}, {1}, {2}, {3}".format(company1_price, company2_price, company3_price, company4_price))
+    input()
+    flutuation_test()
+
+
+if __name__ == "__main__":
+    flutuation_test()
--- stock_market_simulator.py
+++ stock_market_simulator.py
@@ -9,23 +9,58 @@ import random
 
 user_money = float(0)
 user_shares = float(0)
+share_company1 = float(10)
+
+def buying_stocks():
+    global user_money
+    global user_shares
+    global share_company1
+
+    share_company1 = share_company1 + random.randint(-1000, 1000) / 100
+
+    share = input("\nWich share do you want to bid in?")
+
+    if share == "company1":
+        print("Share value: ${:,.2f}".format(share_company1))
+        number_of_shares_to_buy = int(input("How many shares do you want?"))
+        price_of_bid = float(input("For how much each share?"))
+        if price_of_bid >= share_company1:
+            user_money = user_money - (number_of_shares_to_buy * price_of_bid)
+            user_shares = number_of_shares_to_buy + user_shares
+            print("comfirmed transaction")
+    else:
+        print("This company does not exist.")
+        buying_stocks()
+
+    broker()
 
 
 def broker():
     global user_money
     global user_shares
+    global share_company1
 
-    share = input("\nWich share do you want to bid in?")
-    number_of_shares_to_buy = int(input("How many shares do you want?"))
-    price_of_bid = float(input("For how much each share?"))
-    user_money = user_money - (number_of_shares_to_buy * price_of_bid)
-    user_shares = number_of_shares_to_buy + user_shares
+    share_company1 = share_company1 + random.randint(-1000, 1000) / 100
 
-    main()
+    command = input("\nCommand for broker:")
+
+    if command == "buy stocks":
+        buying_stocks()
+    elif command == "sell stocks":
+        main()
+    elif command == "list stocks":
+        print("company1: ${:,.2f}".format(share_company1))
+    else:
+        print("Invalid command")
+
+    broker()
 
 
 def withdraw():
     global user_money
+    global share_company1
+
+    share_company1 = share_company1 + random.randint(-1000, 1000) / 100
 
     money_to_subtract = float(input(
         "\nHow much money do you want to withdraw from this account?"))
@@ -37,6 +72,9 @@ def withdraw():
 
 def transfer():
     global user_money
+    global share_company1
+
+    share_company1 = share_company1 + random.randint(-1000, 1000) / 100
 
     money_to_add = float(input(
         "\nHow much money do you want to transfer for this account?"))
@@ -49,24 +87,35 @@ def transfer():
 def main():
     global user_money
     global user_shares
+    global share_company1
+    user_shares_value = 0
+    total_user_money = 0
     command = 0
+
+    share_company1 = share_company1 + random.randint(-1000, 1000) / 100
+    user_shares_value = share_company1 * user_shares
+    total_user_money = user_money + user_shares_value
 
     print("\nYour portifilio:")
     print("Money: ${:,.2f}".format(user_money))
     print("Shares: {}".format(user_shares))
+    print("Shares value: ${:,.2f}".format(user_shares_value))
+    print("Total: ${:,.2f}".format(total_user_money))
 
     command = input("\nWaiting for command: ")
 
     if command == "broker":
         broker()
-
-    if command == "transfer":
+    elif command == "transfer":
         transfer()
-
-    if command == "withdraw":
+    elif command == "withdraw":
         withdraw()
-
-    print("Invalid command")
+    elif command == "company1 price":
+        print("company1: ${:,.2f}".format(share_company1))
+    elif command == "ls":
+        print("broker\ntransfer\nwithdraw\ncompany1 price\nls")
+    else:
+        print("Invalid command")
 
     main()
 
