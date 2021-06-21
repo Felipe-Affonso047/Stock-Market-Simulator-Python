292e9059ea2d171f5800a887e1f53f817d326a93
YK <felipe.affonso@mths.ca>
2021-06-21 16:16:35 -0400

    Buy and Sell buttons visual

--- final_project/stock.py
+++ final_project/stock.py
@@ -6,17 +6,21 @@
 
 import random
 
+portfolio = {
+    "cash": 0, "total": 0
+}
+
 company1 = {
-    "price": 10, "name": "TSL4", "trend": 1, "fr": 1000
+    "price": 10, "name": "TSL4", "trend": 0.5, "fr": 1000, "users": 0
 }
 company2 = {
-    "price": 5, "name": "P3TR", "trend": 0.5, "fr": 1000
+    "price": 5, "name": "P3TR", "trend": 0.25, "fr": 100, "users": 0
 }
 company3 = {
-    "price": 50, "name": "BK00", "trend": -0.1, "fr": 10000
+    "price": 50, "name": "BK00", "trend": -0.1, "fr": 100, "users": 0
 }
 company4 = {
-    "price": 40, "name": "B4NC", "trend": 0, "fr": 1000
+    "price": 40, "name": "B4NC", "trend": 0, "fr": 10, "users": 0
 }
 
 company_list = [company1] + [company2] + [company3] + [company4]
@@ -30,6 +34,14 @@ def decimal_rounding(numbers, decimals):
     return rounded_number
 
 
+def weird_decimal_rounding(numbers, decimals):
+    # this function rounds decimals
+    rounding_float = float((numbers * pow(10, decimals)) + 0.9999)
+    rounding_int = int(rounding_float)
+    rounded_number = rounding_int / pow(10, decimals)
+    return rounded_number
+
+
 def fluctuation():
     global company_list
 
@@ -37,3 +49,4 @@ def fluctuation():
         company_list[company]["price"] += company_list[company]["trend"] + random.randint(-(company_list[company]["fr"]), company_list[company]["fr"]) / 100
         if company_list[company]["price"] <= 0:
             company_list[company]["price"] += random.randint(0, 9) - company_list[company]["price"]
+        company_list[company]["price"] = decimal_rounding(company_list[company]["price"], 2)
--- final_project/stock_market_simulator_gui.py
+++ final_project/stock_market_simulator_gui.py
@@ -8,25 +8,129 @@ from tkinter import *
 import time
 
 
-def broker(cash):
+def broker(inicial_cash):
     import stock
+
+    stock.portfolio["cash"] = inicial_cash
+
+    def buy_stocks():
+        buy_stocks_scene = Toplevel()
+
+        def buy_logic():
+            buy_stocks_scene.destroy()
+            broker_scene.destroy()
+    
+        stock_name = StringVar()
+        quantity = StringVar()
+
+        stock_name_text = Label(buy_stocks_scene, text="Insert the name of the stock you want to buy (example: TSL4):")
+        stock_name_inser = Entry(buy_stocks_scene, border=5, font=("", 20), textvariable=stock_name)
+        quantity_text = Label(buy_stocks_scene, text="Insert the quantity of stocks you want to buy:")
+        quantity_inser = Entry(buy_stocks_scene, border=5, font=("", 20), textvariable=quantity)
+        buy_button_2 = Button(buy_stocks_scene, text="BUY", font=("", 20), command=buy_logic, background="#01C801")
+
+        stock_name_text.pack()
+        stock_name_inser.pack()
+        quantity_text.pack()
+        quantity_inser.pack()
+        buy_button_2.pack()
+
+        buy_stocks_scene.mainloop()
+        return
+
+    def sell_stocks():
+        sell_stocks_scene = Toplevel()
+
+        def sell_logic():
+            sell_stocks_scene.destroy()
+            broker_scene.destroy()
+    
+        stock_name = StringVar()
+        quantity = StringVar()
+
+        stock_name_text = Label(sell_stocks_scene, text="Insert the name of the stock you want to sell (example: TSL4):")
+        stock_name_inser = Entry(sell_stocks_scene, border=5, font=("", 20), textvariable=stock_name)
+        quantity_text = Label(sell_stocks_scene, text="Insert the quantity of stocks you want to sell:")
+        quantity_inser = Entry(sell_stocks_scene, border=5, font=("", 20), textvariable=quantity)
+        buy_button_2 = Button(sell_stocks_scene, text="SELL", font=("", 20), command=sell_logic, background="#960132")
+
+        stock_name_text.pack()
+        stock_name_inser.pack()
+        quantity_text.pack()
+        quantity_inser.pack()
+        buy_button_2.pack()
+
+        sell_stocks_scene.mainloop()
+
     broker_scene = Tk()
 
-    while True:
-        for widgets in broker_scene.winfo_children():
-            widgets.destroy()
+    boolean = []
+    boolean.append(True)
+
+    while boolean[0] == True:
+        def exit():
+            boolean[0] = False
+        # save the company prices from before
+        temp_companies = []
+        for company in range(len(stock.company_list)):
+            if len(temp_companies) != len(stock.company_list):
+                temp_companies.append(stock.company_list[company]["price"])
+            else:
+                temp_companies[company] = stock.company_list[company]["price"]
+
         stock.fluctuation()
 
-        text_color = "green"
+        try:
+            for widgets in broker_scene.winfo_children():
+                widgets.destroy()
+        except:
+            broker_scene = Tk()
+
+        total_text = "TOTAL: " + str(stock.portfolio["total"])
+        cash_text = "CASH: " + str(stock.portfolio["cash"])
+
+        total_label = Label(text=total_text)
+        cash_label = Label(text=cash_text)
+        buy_button = Button(text="BUY", height=3, background="#01C801", command=buy_stocks)
+        sell_button = Button(text="SELL", height=3, background="#960132", command=sell_stocks)
+        exit_button = Button(text="exit", command=exit)
+
+        total_label.grid(row=0, column=1, columnspan=2)
+        cash_label.grid(row=0, column=3, columnspan=2)
+        buy_button.grid(row=1, column=0)
+        sell_button.grid(row=2, column=0, rowspan=2)
+        exit_button.grid(row=0, column=0)
 
         for company in range(len(stock.company_list)):
-            text = stock.company_list[company]["name"] + "\n" + str(stock.company_list[company]["price"])
-            company_text = Label(broker_scene, text=text, background="#000000", fg=text_color, font=("", 20))
-            company_text.grid(row=1, column=company)
+            # this loop deal with all the companies
+            if temp_companies[company] < stock.company_list[company]["price"]:
+                text_color = "#01FF01"
+            else:
+                text_color = "#FF0101"
 
-        broker_scene.update_idletasks()
-        broker_scene.update()
-        time.sleep(0.5)
+            text = stock.company_list[company]["name"] + "\n" + str(stock.company_list[company]["price"])
+            shares_text = "Shares: " + str(stock.company_list[company]["users"])
+            shares_value = stock.company_list[company]["users"] * stock.company_list[company]["price"]
+            shares_value_text = "Value: " + str(shares_value)
+
+            company_text = Label(broker_scene, text=text, background="#000000", fg=text_color, font=("", 20), width=7)
+            user_company_shares = Label(text=shares_text)
+            user_company_value = Label(text=shares_value_text)
+
+            column = company + 1
+            company_text.grid(row=1, column=column)
+            user_company_shares.grid(row=2, column=column)
+            user_company_value.grid(row=3, column=column)
+
+        for counter in range(10):
+            try:
+                broker_scene.update_idletasks()
+                broker_scene.update()
+            except:
+                broker_scene = Tk()
+            time.sleep(0.1)
+
+    broker_scene.destroy()
 
 
 def money_printer():
@@ -35,16 +139,16 @@ def money_printer():
     answer = StringVar()
 
     def submit():
-        try:
+        #try:
             result = float(answer.get())
             money_printer_scene.destroy()
             return broker(result)
-        except Exception:
-            error = Label(text="ERROR\nTry entering a number.", font=("", 20), background="#FF0101")
-            error.grid()
+        #except Exception:
+            #error = Label(text="ERROR\nTry entering a number.", font=("", 20), background="#FF0101")
+            #error.grid()
 
     question = Label(text="How much money you want to start with?", font=("", 20), background="#01C801")
-    response_box = Entry(text="ENTER A NUMBER", border=5, font=("", 20), textvariable=answer)
+    response_box = Entry(border=5, font=("", 20), textvariable=answer)
     submit_num = Button(text="SUBMIT", width=20, font=("", 12), border=5, background="#960132", command=submit)
 
     question.grid(row=0, column=0, columnspan=2)
@@ -53,11 +157,10 @@ def money_printer():
 
     money_printer_scene.mainloop()
 
-
 def start():
     def start_button_fun():
         start_scene.destroy()
-        return money_printer()
+        return
 
     start_scene = Tk()
 
@@ -68,7 +171,7 @@ def start():
     start_button.grid(row=1, column=1)
 
     start_scene.mainloop()
-
+    return money_printer()
 
 if __name__ == "__main__":
     start()
